6dad97549e1dfd202de5c81f79e7264b9f26f434
Pavel Tisunov <tisunov.pavel@gmail.com>
2014-01-02 15:16:24 +0400

    Environment settings

--- groundControlApi.js
+++ groundControlApi.js
@@ -1,15 +1,13 @@
 var Driver = require("./models/driver").Driver,
 		Client = require("./models/client").Client,
 		request = require("request"),
-		util = require("util");
+		util = require("util"),
+		CONFIG = require('config').Backend;
 
 function GroundControlAPI() { 
 }
 
-// var users = [
-// 	{ id : 3144718, "token" : "83fdd5c78b14", "email" : "igor@mail.ru", "firstName" : "Игорь", "mobile" : "+79207845467", "rating" : 4.8 },
-// 	{ id : 3144719, "token" : "a6f4f35a486e", "email" : "pavel@mail.ru", "firstName" : "Павел", "mobile" : "+79204567890", "rating" : 4.9 },
-// ];
+var backendUrl = 'http://' + CONFIG.apiHost + ':' + CONFIG.apiPort;
 
 function initProperties(sourceProps) {
 	Object.keys(sourceProps).forEach(function(propName) {
@@ -44,11 +42,11 @@ function login(url, email, password, constructor, callback) {
 }
 
 GroundControlAPI.loginDriver = function(email, password, callback) {
-	login('http://localhost:3000/api/v1/drivers/sign_in', email, password, Driver, callback);
+	login(backendUrl + '/api/v1/drivers/sign_in', email, password, Driver, callback);
 }
 
 GroundControlAPI.loginClient = function(email, password, callback) {
-	login('http://localhost:3000/api/v1/sign_in', email, password, Client, callback);
+	login(backendUrl + '/api/v1/sign_in', email, password, Client, callback);
 }
 
 GroundControlAPI.completeTrip = function(trip, callback) {
