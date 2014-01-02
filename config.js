3de4d184d58e14ae89ee78e2cdc1bf5905bf9e17
Pavel Tisunov <tisunov.pavel@gmail.com>
2014-01-02 15:43:38 +0400

    RedisToGo specific config

--- config.js
+++ config.js
@@ -1,4 +1,12 @@
-var redis = require("redis").createClient();
+var redis
+
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  redis = require("redis").createClient();
+}
 
 redis.on("error", function (err) {
    console.log("Error " + err);
